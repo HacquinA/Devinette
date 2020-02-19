1c28a0baf6e2aa32e7aa454f06c52740c6ceed69
AthenaisHacquinETPA <58641398+HacquinA@users.noreply.github.com>
2020-02-20 00:48:33 +0100

    devientte 1

---
+++ devinette.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int poJoueur = 10;
+int ptdVie = 100;
+
+
+int main(){
+
+int choix = 0;
+
+
+//Tutos 
+printf("---------------Tutoriel---------------\n\n");
+printf("Hey bro ! Bienvenue dans un jeu de devinette ! Eh ouais ici tu vas devoir repondre a plusieurs devinette si tu veux devenir le ou la king !\n");
+printf(" Jte fait le topo. T'a trois chance ! Pas une de plus je te previens , tu ne pourras pas m'acheter !\n");
+printf(" Si jamais t es vraiment dans la galere tu auras le droit a un joker dans la parti, grace a lui tu auras deux choix de reponses au lieu de 4\n");
+printf(" Je t ai meme pas dit le plus important ! Avant chaque réponse tu auras le droit de repondre direct ou de choisir les 4 propositions, sachant que si tu reponds cache tu gagnes 10 points au lieu de 5 !\n");
+
+// Start
+
+printf("---------------C est parti---------------\n\n");
+printf("---------------Devinette n°1---------------\n\n");
+printf("Vous avancez dans un couloir et il ne vous est pas possible de revenir en arriere. Soudain vous vous retrouvez en face de trois portes.\n\n- Derriere la premiere il y a un assassin sanguinaire arme de couteaux de boucherie.\n- Derriere la deuxieme il y a un lion qui n'a pas mange depuis 6 mois.\n- Derriere la troisieme il y a un trou de 100 metres de haut et infranchissable.\nQuel choix allez-vous faire pour pouvoir survivre a cette aventure?\n");
+printf("!!reponse!!\n\n");
+printf("(1) Porte avec l'assassin, (2) Porte avec le Lion, (3) Porte vec le trou, (4) joker\n");
+scanf("")
+
+
+		
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
