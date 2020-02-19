35e471a870d6e632d967fda0f3771f6af6c3e892
AthenaisHacquinETPA <58641398+HacquinA@users.noreply.github.com>
2020-02-20 00:47:01 +0100

    tuto

---
+++ devinette.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int poJoueur = 10;
+int ptdVie = 100;
+
+
+int main(){
+
+int choix = 0;
+
+
+//Tutos 
+printf("---------------Tutoriel---------------\n\n");
+printf("Hey bro ! Bienvenue dans un jeu de devinette ! Eh ouais ici tu vas devoir repondre a plusieurs devinette si tu veux devenir le ou la king !\n");
+printf(" Jte fait le topo. T'a trois chance ! Pas une de plus je te previens , tu ne pourras pas m'acheter !\n");
+printf(" Si jamais t es vraiment dans la galere tu auras le droit a un joker dans la parti, grace a lui tu auras deux choix de reponses au lieu de 4\n");
+printf(" Je t ai meme pas dit le plus important ! Avant chaque réponse tu auras le droit de repondre direct ou de choisir les 4 propositions, sachant que si tu reponds cache tu gagnes 10 points au lieu de 5 !\n");
+
+
+
+		
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
